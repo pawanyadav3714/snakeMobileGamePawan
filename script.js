9e31c4a48d4ce809e2d2cf384805f1467b88939d
pawanyadav3714 <pawanyadav3714@gmail.com>
2026-03-03 19:31:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ let score = 0;
 let lastPaintTime = 0;
 
 let snakeArr = [
-    {x:16, y:24}
+    {x:37, y:23}
 ]
-food = {x:33, y:17};
+food = {x:43, y:37};
 
     //game functions
 function main(ctime){
@@ -33,7 +33,7 @@ function isCollide(snake){
         }
         }
         // If you bump into the wall
-        if(snake[0].x >= 50 || snake[0].x < 0 || snake[0].y >= 50 || snake[0].y < 0){
+        if(snake[0].x >= 70 || snake[0].x < 0 || snake[0].y >= 70 || snake[0].y < 0){
             gameOverSound.play();
         return true;
         }
@@ -65,8 +65,8 @@ function gameEngine(){
             hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
         }
         scoreBox.innerHTML = "Score: " + score;
-        let a = 2;
-        let b = 49;
+        let a = 1;
+        let b = 69;
         food = {x: Math.round(a + (b-a)*Math.random()),
                 y: Math.round(a + (b-a)*Math.random())}
     }
@@ -172,4 +172,3 @@ document.querySelector('#right').addEventListener('touchstart', () => {
     inputdirection.y = 0;
     moveSound.play();
 })
-
